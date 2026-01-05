daa87024da827e188c893c5db56e7b831a69c7a8
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-05 02:53:29 +0200

    main structure

---
+++ src/features/translate-page/translate-page.component.ts
@@ -0,0 +1,37 @@
+import './translate-page.component.css';
+
+function translatePageComponent() {
+  const app = document.querySelector('#app');
+
+  if(!app) {
+    throw new Error('App element not found');
+  }
+
+  app.innerHTML = `
+    <section>
+      <div class="field-container">
+        <label for="select-original-language">Оберіть мову оригіналу</label>
+        <select id="select-original-language"></select>
+      </div>
+      
+      <div class="field-container">
+        <label for="select-translate-language">Оберіть мову перекладу</label>
+        <select id="select-translate-language"></select>
+      </div>
+    </section>
+
+    <section>
+      <div class="field-container">
+        <label for="original-text">Введіть текст</label>
+        <textarea id="original-text"></textarea>
+      </div>
+      
+      <div class="field-container">
+        <label for="translate-text">Переклад</label>
+        <textarea readonly id="translate-text"></textarea>
+      </div>
+    </section>
+  `
+}
+
+translatePageComponent();
