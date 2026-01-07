f3f0d1d2c5e7cbb83653f8867b10a37db16270e7
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-07 02:32:41 +0200

    add bootstrap

--- src/features/translate-page/application/provide-view.ts
+++ src/features/translate-page/application/provide-view.ts
@@ -4,36 +4,36 @@ export function provideTranslatePageView(app: Element) {
   app.innerHTML = `
     <form class="main-center w-100">
       <div class="w-100">
-        <label for="${TranslatePageElementIds.AutoTranslateCheckbox}">Автопереклач без кнопки</label>
-        <input id="${TranslatePageElementIds.AutoTranslateCheckbox}" type="checkbox">
+        <label class="form-check-label" for="${TranslatePageElementIds.AutoTranslateCheckbox}">Автопереклач без кнопки</label class="form-label">
+        <input class="form-check-input" id="${TranslatePageElementIds.AutoTranslateCheckbox}" type="checkbox">
       </div>
   
       <section>
         <div class="field-container">
-          <label for="${TranslatePageElementIds.SelectOriginalLanguage}">Оберіть мову оригіналу</label>
-          <select required id="${TranslatePageElementIds.SelectOriginalLanguage}"></select>
+          <label class="form-label" for="${TranslatePageElementIds.SelectOriginalLanguage}">Оберіть мову оригіналу</label class="form-label">
+          <select class="form-select" required id="${TranslatePageElementIds.SelectOriginalLanguage}"></select>
         </div>
         
         <div class="field-container">
-          <label for="${TranslatePageElementIds.SelectTranslateLanguage}">Оберіть мову перекладу</label>
-          <select required id="${TranslatePageElementIds.SelectTranslateLanguage}"></select>
+          <label class="form-label" for="${TranslatePageElementIds.SelectTranslateLanguage}">Оберіть мову перекладу</label class="form-label">
+          <select class="form-select" required id="${TranslatePageElementIds.SelectTranslateLanguage}"></select>
         </div>
       </section>
   
       <section>
         <div class="field-container">
-          <label for="${TranslatePageElementIds.OriginalTextarea}">Введіть текст</label>
-          <textarea required class="w-100" id="${TranslatePageElementIds.OriginalTextarea}"></textarea>
+          <label class="form-label" for="${TranslatePageElementIds.OriginalTextarea}">Введіть текст</label class="form-label">
+          <textarea class="form-control" required class="w-100" id="${TranslatePageElementIds.OriginalTextarea}"></textarea>
         </div>
         
         <div class="field-container">
-          <label for="${TranslatePageElementIds.TranslateTextarea}">Переклад</label>
-          <textarea readonly class="w-100" id="${TranslatePageElementIds.TranslateTextarea}"></textarea>
+          <label class="form-label" for="${TranslatePageElementIds.TranslateTextarea}">Переклад</label class="form-label">
+          <textarea class="form-control" readonly class="w-100" id="${TranslatePageElementIds.TranslateTextarea}"></textarea>
         </div>
       </section>
       
       <div class="w-100">
-        <button type="submit">Перекласти</button>
+        <button class="btn btn-light" type="submit">Перекласти</button>
       </div>
     </form>
   `
