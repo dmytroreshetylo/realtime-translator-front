16f46ea088260e5c86f267146acc75b76b1e4c8b
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-08 04:21:40 +0200

    feat: update language codes in LanguageService for standardization

--- src/domain/languages/language.service.ts
+++ src/domain/languages/language.service.ts
@@ -2,16 +2,16 @@ import type { LanguageModel } from './language.model.ts';
 
 export class LanguageService {
   private readonly list: LanguageModel[]  = [
-    { code: 'english', name: 'Англійська' },
-    { code: 'french', name: 'Французька' },
-    { code: 'german', name: 'Німецька' },
-    { code: 'spanish', name: 'Іспанська' },
-    { code: 'italian', name: 'Італійська' },
-    { code: 'portuguese', name: 'Португальська' },
-    { code: 'ukrainian', name: 'Українська' },
-    { code: 'chinese', name: 'Китайська' },
-    { code: 'japanese', name: 'Японська' },
-    { code: 'korean', name: 'Корейська' }
+    { code: 'en', name: 'Англійська' },
+    { code: 'fr', name: 'Французька' },
+    { code: 'de', name: 'Німецька' },
+    { code: 'es', name: 'Іспанська' },
+    { code: 'it', name: 'Італійська' },
+    { code: 'pt', name: 'Португальська' },
+    { code: 'uk', name: 'Українська' },
+    { code: 'zh', name: 'Китайська' },
+    { code: 'ja', name: 'Японська' },
+    { code: 'ko', name: 'Корейська' }
   ]
 
   getLanguages() {
