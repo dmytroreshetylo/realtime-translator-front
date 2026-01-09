c57eceab893b0f54acee0369d9afd55b0e986a93
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-09 06:20:46 +0200

    fix bugs

--- src/shared/services/localstorage.service.ts
+++ src/shared/services/localstorage.service.ts
@@ -1,21 +1,12 @@
 export class LocalStorageService {
   get<T>(key: string): T | null {
-    try {
-      const data = localStorage.getItem(key);
-      return data ? JSON.parse(data) : null;
-    } catch (error) {
-      console.error(`Error getting data from localStorage for key "${key}"`, error);
-      return null;
-    }
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
   }
 
   set<T>(key: string, value: T): void {
-    try {
-      const data = JSON.stringify(value);
-      localStorage.setItem(key, data);
-    } catch (error) {
-      console.error(`Error setting data to localStorage for key "${key}"`, error);
-    }
+    const data = JSON.stringify(value);
+    localStorage.setItem(key, data);
   }
 
   remove(key: string): void {
